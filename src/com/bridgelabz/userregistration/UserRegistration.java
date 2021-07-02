610abf334daa78bde65bc5ffdcc87c917c19e41c
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-02 17:39:41 +0530

    Added: Welcome to user registration problem

---
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.userregistration;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to user registration problem :");
+		
+	}
+
+}
+
+
