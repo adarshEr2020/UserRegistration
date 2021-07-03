eea5a1b1ddd992bec72188060afb63b064b453a8
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-03 12:01:04 +0530

    Added: validation user last name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -16,13 +16,25 @@ public class UserRegistration {
 		return  matcher.matches();
 	}
 
+	public boolean lastName() {
+
+		System.out.println("Enter last name : ");
+		String lastName = scan.next();
+		String namePatten = "^[A-Z]{1}[a-z]{2}";
+		Pattern pattern = Pattern.compile(namePatten);
+		Matcher matcher = pattern.matcher(lastName);
+		return  matcher.matches();
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to user registration problem :");
 		UserRegistration user = new UserRegistration();
 		boolean result1 = user.firstName();
 		System.out.println("first name" +result1);
-		
+		boolean result2 = user.lastName();
+		System.out.println("last name " +result2);
+
 	}
 
 }
