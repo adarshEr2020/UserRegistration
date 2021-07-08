631c0ac9062422ffcced7f465b3efd51db7aacde
adarshEr2020 <adarshprajapati31@gmail.com>
2021-07-08 12:50:04 +0530

    [Adarsh] Added mobile format validation

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,11 +1,21 @@
 package com.bridgelabz.userregistration;
 
+/*User Registration
+ * 
+ *User Registration System needs to ensure all validations
+ *
+ *are in place during the User Entry
+ *
+ */
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 	Scanner scan = new Scanner(System.in);
+
+	// First name validation 
 	public boolean firstName() {
 
 		System.out.println("Enter first name : ");
@@ -16,6 +26,7 @@ public class UserRegistration {
 		return  matcher.matches();
 	}
 
+	// Last name validation  
 	public boolean lastName() {
 
 		System.out.println("Enter last name : ");
@@ -25,28 +36,44 @@ public class UserRegistration {
 		Matcher matcher = pattern.matcher(lastName);
 		return  matcher.matches();
 	}
-	
+
+	// Email validation 
 	public boolean validEmail() {
 		System.out.println("Enter email : ");
 		String userEmail = scan.next();
-		String emailPattern = "^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$";
+		String emailPattern ="^([0-9]{2})?[0-9]{10}" ;
 		Pattern pattern = Pattern.compile(emailPattern);
 		Matcher matcher = pattern.matcher(userEmail);
 		return matcher.matches();
 	}
 
+	// Mobile format validation 
+	public boolean mobileFormat() {
+		System.out.println("Enter mobile no with country : ");
+		String mobile = scan.next();
+		String mobilePattern ="^(0|91)?[6-9]{1}[0-9]{9}$";
+		Pattern pattern = Pattern.compile(mobilePattern);
+		Matcher matcher = pattern.matcher(mobile);
+		return matcher.matches();
+	}
+
+	// Main method here 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to user registration problem :");
 		UserRegistration user = new UserRegistration();
+		
 		boolean result1 = user.firstName();
 		System.out.println("first name" +result1);
+		
 		boolean result2 = user.lastName();
 		System.out.println("last name " +result2);
+		
 		boolean result3 = user.validEmail();
 		System.out.println("last name " +result3);
+		
+		boolean result4 = user.mobileFormat();
+		System.out.println("Mobile format validation " +result4);
 	}
 
 }
-
-
